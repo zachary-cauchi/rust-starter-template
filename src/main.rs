aa58bb76e1c7cac6f3d728b98cac1957849b8dde
Zachary Cauchi <zach.cauchi@hotmail.com>
2023-12-26 21:14:37 +0100

    feat: add reloading of log configuration

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
+use std::error::Error;
+
+use configuration::{app_config::AppConfig, AppConfigManager};
 use tokio::task::JoinSet;
 use tracing::{debug, error, info, span, Instrument, Level};
 use utils::logging::LogSubscriberBuilder;
 
 #[tokio::main]
-async fn main() {
-    LogSubscriberBuilder::new()
-        .with_max_level(Level::DEBUG)
-        .build_global();
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut log_manager = LogSubscriberBuilder::new().with_fmt_logging(Level::INFO);
+
+    log_manager.build();
 
     info!("Hello World");
+
+    let app_config: AppConfig = AppConfigManager::clone_to_app_config().unwrap();
+
+    log_manager
+        .with_fmt_logging_str(&app_config.logging.cli_log_level)
+        .refresh()?;
 
     const TOTAL_TASKS: usize = 14;
 
@@ -31,4 +40,6 @@ async fn main() {
             Err(e) => error!("Failed to process task: {}", e),
         }
     }
+
+    Ok(())
 }
--- utils/src/logging.rs
+++ utils/src/logging.rs
@@ -1,14 +1,24 @@
+use std::error::Error;
+
 use tracing::Level;
+use tracing_subscriber::{
+    filter,
+    fmt::{self},
+    prelude::*,
+    reload, Registry,
+};
 
 #[derive(Debug)]
 pub struct LogSubscriberBuilder {
-    log_level: Level,
+    fmt_log_level: Option<Level>,
+    fmt_layer_reload_handle: Option<reload::Handle<filter::LevelFilter, Registry>>,
 }
 
 impl Default for LogSubscriberBuilder {
     fn default() -> Self {
         Self {
-            log_level: Level::INFO,
+            fmt_log_level: Some(Level::INFO),
+            fmt_layer_reload_handle: None,
         }
     }
 }
@@ -18,16 +28,56 @@ impl LogSubscriberBuilder {
         Self::default()
     }
 
-    pub fn with_max_level(mut self, level: Level) -> Self {
-        self.log_level = level;
+    pub fn with_fmt_logging(mut self, level: Level) -> Self {
+        self.fmt_log_level = Some(level);
+
         self
     }
 
-    pub fn build_global(self) {
-        tracing_subscriber::fmt()
-            .with_max_level(self.log_level)
-            .init()
+    pub fn with_fmt_logging_str(self, level_str: &str) -> Self {
+        match Self::level_from_str(level_str) {
+            Some(level) => self.with_fmt_logging(level),
+            None => self,
+        }
+    }
+
+    fn level_from_str(level_str: &str) -> Option<Level> {
+        match level_str.to_uppercase().as_str() {
+            "INFO" => Some(Level::INFO),
+            "DEBUG" => Some(Level::DEBUG),
+            "WARN" => Some(Level::WARN),
+            "ERROR" => Some(Level::ERROR),
+            "TRACE" => Some(Level::TRACE),
+            _ => None,
+        }
+    }
+
+    /// Set the global logging filters.
+    /// Can only be called once.
+    pub fn build(&mut self) {
+        let registry = tracing_subscriber::registry();
+
+        if let Some(log_level) = self.fmt_log_level {
+            let (filter, reload_handle) =
+                reload::Layer::new(filter::LevelFilter::from_level(log_level));
+            self.fmt_layer_reload_handle = Some(reload_handle);
+
+            let registry = registry.with(filter).with(fmt::Layer::default());
+
+            registry.init();
+        } else {
+            panic!("Cannot initialise logging without fmt_params.")
+        }
+    }
+
+    /// Refresh the global subscribers with any updated filters.
+    pub fn refresh(&mut self) -> Result<(), Box<dyn Error>> {
+        if let Some(reload_handle) = &self.fmt_layer_reload_handle {
+            reload_handle.modify(|filter| {
+                *filter = filter::LevelFilter::from_level(self.fmt_log_level.unwrap())
+            })?;
+        }
+
+        Ok(())
     }
 }
-
-pub fn init_log_subscriber() {}
