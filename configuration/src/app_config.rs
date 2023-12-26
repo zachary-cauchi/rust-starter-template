be0c5dce60578822c2006b52641691a26025205d
Zachary Cauchi <zach.cauchi@hotmail.com>
2023-12-26 10:52:33 +0100

    feat: add configuration package

---
+++ configuration/src/app_config.rs
@@ -0,0 +1,32 @@
+use config::Config;
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+pub struct Database {
+    pub name: String,
+    pub url: String,
+}
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+pub struct Logging {
+    pub cli_log_level: String,
+}
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+pub struct AppConfig {
+    pub logging: Logging,
+    pub databases: Vec<Database>,
+}
+
+impl From<Config> for AppConfig {
+    fn from(config: Config) -> Self {
+        AppConfig {
+            logging: config
+                .get::<Logging>("logging")
+                .expect("No valid logging configuration found."),
+            databases: config
+                .get::<Vec<Database>>("database")
+                .expect("No valid database configuration found."),
+        }
+    }
+}
---
+++ configuration/src/lib.rs
@@ -0,0 +1,50 @@
+use std::error::Error;
+
+use app_config::AppConfig;
+use config::Config;
+use lazy_static::lazy_static;
+use parking_lot::RwLock;
+
+pub mod app_config;
+
+lazy_static! {
+    /// The Global default config. Any default start-time configuration should be set here.
+    /// A default configuration is hard-coded at compile-time using `include_str!`, then environment overrides are loaded.
+    static ref CONFIG: RwLock<Config> = RwLock::new(
+        Config::builder()
+            .add_source(config::File::from_str(
+                include_str!("../../configs/default_config.toml"),
+                config::FileFormat::Toml
+            ))
+            .add_source(config::Environment::with_prefix("RUST_STARTER_TEMPLATE"))
+            .build()
+            .unwrap()
+    );
+}
+
+/// The main configuration manager for the application. All config changes should go through here.
+pub struct AppConfigManager {}
+
+impl AppConfigManager {
+    pub fn set(key: &str, value: &str) -> Result<(), Box<dyn Error>> {
+        //TODO: Replace deprecated Config::set method.
+        CONFIG.write().set(key, value)?;
+
+        Ok(())
+    }
+
+    pub fn get<'de, T>(key: &'de str) -> Result<T, Box<dyn Error>>
+    where
+        T: serde::Deserialize<'de>,
+    {
+        Ok(CONFIG.read().get::<T>(key)?)
+    }
+
+    pub fn clone_to_app_config() -> Result<AppConfig, Box<dyn Error>> {
+        let c = CONFIG.read().clone();
+
+        let app_config: AppConfig = c.into();
+
+        Ok(app_config)
+    }
+}
