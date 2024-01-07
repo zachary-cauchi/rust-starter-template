b4f596b00cc16564014eb7aedcb23bac2591b39d
Zachary Cauchi <zach.cauchi@hotmail.com>
2024-01-07 22:11:28 +0100

    feat: remove old example lib func

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -2,18 +2,3 @@ pub mod core_types;
 pub mod logging;
 pub mod panic;
 pub mod project_info;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
